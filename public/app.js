19d54db23508dcc972c2ed5ae990c429147daccb
Campbell Miller <campbellmiller4e@gmail.com>
2018-07-30 22:28:20 +0100

    adds more cats/css

--- public/app.js
+++ public/app.js
@@ -1,9 +1,12 @@
 var CAT_ARRAY = [
-  {name: "Spooky", favFood: "Ham", image: "http://gifimage.net/wp-content/uploads/2017/10/cat-pixel-gif-7.gif"},
+  {name: "Spooky", favFood: "Fried Chicken", image: "http://gifimage.net/wp-content/uploads/2017/10/cat-pixel-gif-7.gif"},
   {name: "Clorp", favFood: "Sandwiches", image: "https://is3-ssl.mzstatic.com/image/thumb/Purple115/v4/09/2a/e3/092ae356-b1f5-55f2-31e8-14d619522a3b/AppIcon-1x_U007emarketing-85-220-0-5.png/246x0w.jpg"},
   {name: "Dr. Butts", favFood: "Pierogi", image: "https://i.pinimg.com/236x/65/bf/29/65bf29e93f43671504b5f80beb783414--animal-jam-pixel-art.jpg"},
   {name: "Keith", favFood: "Candyfloss", image: "https://image.shutterstock.com/image-vector/vector-illustration-cartoon-cat-pixel-260nw-387938092.jpg"},
-  {name: "Pusheen", favFood: "Pizza", image: "https://icon2.kisspng.com/20180508/exe/kisspng-cat-pixel-art-pusheen-5af1d0f8983341.4977117815257971126234.jpg"}
+  {name: "Pusheen", favFood: "Pizza", image: "https://i.gifer.com/XWeP.gif"},
+  {name: "Treacle", favFood: "Cake", image: "https://pbs.twimg.com/profile_images/993230057240920065/omAKzSQ6_400x400.jpg"},
+  {name: "Puff", favFood: "Mackerel", image: "https://media.giphy.com/media/ea74cjF0jieXu/giphy.gif"},
+  {name: "Bert", favFood: "Sushi", image: "https://res.cloudinary.com/teepublic/image/private/s--RDXQX9ci--/t_Preview/b_rgb:ffffff,c_limit,f_jpg,h_630,q_90,w_630/v1517094435/production/designs/2314766_0.jpg"}
 
 ]
 
@@ -30,7 +33,7 @@ let createCatName = function(name){
 
 let createCatFood = function(favFood){
   let catfood = document.createElement('li');
-  catfood.innerText = `Favourite Food: ${favFood}`;
+  catfood.innerText = `Likes: ${favFood}`;
   return catfood;
 }
 
@@ -38,6 +41,7 @@ let createCatPic = function(image){
   let catpic = document.createElement('img');
   catpic.src = image;
   catpic.width= 200;
+  catpic.height= 200;
   return catpic;
 }
 
